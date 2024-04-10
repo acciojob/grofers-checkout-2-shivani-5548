55b21f7098f7a810eebc8a49a7e7b7d13c9838a3
shivani-5548 <90037384+shivani-5548@users.noreply.github.com>
2024-04-10 16:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const prices = document.querySelectorAll('[data-ns-test="price"]');
+const grandTotal = document.querySelector('[data-ns-test="grandTotal"]');
+
+let total = 0;
+
+prices.forEach(price => {
+  total += parseInt(price.textContent);
+});
+
+grandTotal.textContent = total.toFixed(2);
